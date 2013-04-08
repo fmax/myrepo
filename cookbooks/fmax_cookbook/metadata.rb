491e17d9af868e97806fad8d8c5984e399ef3d50
fmax <markus.froehlich@xidras.com>
2013-04-08 16:43:09 +0000

    added hello world to default.rb recipe

---
+++ cookbooks/fmax_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "YOUR_COMPANY_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures fmax_cookbook"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "fmax_cookbook::default", "prints hello world output"
---
+++ cookbooks/fmax_cookbook/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: fmax_cookbook
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "hello world! you should try chef with rightscale!"
